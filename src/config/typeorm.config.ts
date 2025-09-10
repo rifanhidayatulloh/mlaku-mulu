b9d42a8ced09d63087971a3755bc2f2bb4ede5d2
rifanhidayatulloh <rifanhidayatulloh96@gmail.com>
2025-09-10 23:12:42 +0700

    Update database configuration to use PostgreSQL instead of MySQL

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,9 +4,9 @@ import { User } from '../entities/user.entity';
 import { Trip } from '../entities/trip.entity';
 
 export const typeOrmConfig = (config: ConfigService): DataSourceOptions => ({
-  type: 'mysql',
+  type: 'postgres',
   host: config.get('DATABASE_HOST'),
-  port: parseInt(config.get('DATABASE_PORT') || '3306', 10),
+  port: parseInt(config.get('DATABASE_PORT') || '5433', 10),
   username: config.get('DATABASE_USER'),
   password: config.get('DATABASE_PASSWORD'),
   database: config.get('DATABASE_NAME'),
