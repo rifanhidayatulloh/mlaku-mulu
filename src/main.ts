6a5d5bf503d0d1e09b8e8096852aedc49b232cb8
rifanhidayatulloh <rifanhidayatulloh96@gmail.com>
2025-09-10 14:39:11 +0700

    Add polyfill for randomUUID in global crypto object

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as nodeCrypto from "crypto";
+
+if (!(global as any).crypto?.randomUUID) {
+  (global as any).crypto = {
+    ...(global as any).crypto,
+    randomUUID: nodeCrypto.randomUUID,
+  };
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
