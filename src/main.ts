26112df5abc856c9b6fa16561f8557122fba60bf
rifanhidayatulloh <rifanhidayatulloh96@gmail.com>
2025-09-11 12:51:18 +0700

    Enable CORS for the application with specified origins and methods

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,18 @@ if (!(global as any).crypto?.randomUUID) {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Aktifkan CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:8080',
+      'https://mlaku-mulu-adventure.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(3000);
 }
